45fe457270ce018bdbd4528f9a25659b0d61a96c
Cara Locke <cara.m.locke@gmail.com>
2021-07-15 12:49:21 -0400

    Realized I did the last step in the wrong place.. Also completed Step 4 - Consuming data with ProductContext.

--- src/App.js
+++ src/App.js
@@ -21,21 +21,20 @@ function App() {
 			item
 		])
 	};
-	
+	//1.) Wrap all of your components/routes in `App.js` inside of `ProductContext.Provider` component. 2.) Next pass a value prop to your `Provider`. 3.) In the value prop we'll pass in the products state, and an addItem function that will allow us to add books to the cart.
+	//Now that we're providing our products state and addItem function we can simplify our products route a bit. (Remove props) 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
-			{/* 1.) Wrap all of your components/routes in `App.js` inside of `ProductContext.Provider` component. 2.) Next pass a value prop to your `Provider`. 3.) In the value prop we'll pass in the products state, and an addItem function that will allow us to add books to the cart. */}
-			<ProductContext.Provider value={}>
-				{/* Routes */}
-				<Route exact path="/">
-					{/*  Now that we're providing our products state and addItem function we can simplify our products route a bit. (Remove props) */}
-					<Products />
-				</Route>
+			<ProductContext.Provider value={{products, addItem}}>
+				<Navigation cart={cart} />
+					{/* Routes */}
+					<Route exact path="/">
+						<Products />
+					</Route>
 
-				<Route path="/cart">
-					<ShoppingCart />
-				</Route>
+					<Route path="/cart">
+						<ShoppingCart />
+					</Route>
 			</ProductContext.Provider>
 		</div>
 	);
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,11 @@
+//Now that our `ProductContext` is now providing data we can finally consume it! To do so let's head over to our `Products` component and import the `useContext` hook as well as our `ProductContext`
+import { useContext } from 'react';
+import { ProductContext } from '../contexts/ProductContext';
 import React from 'react';
 
 const Product = props => {
+	//In the component, call the `useContext` hook and pass in the context object we want to use into it.
+	const {product, addItem} = useContext(ProductContext)
 	return (
 		<div className="product">
 			<img src={props.product.image} alt={`${props.product.title} book`} />
