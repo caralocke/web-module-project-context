a6676bb1fbe1c5042ec2e2f856e4c3a48332e14c
Cara Locke <cara.m.locke@gmail.com>
2021-07-15 13:20:51 -0400

    Completed Step 7 - The final stretch.

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{products, addItem}}>
-				<CartContext value={cart}>
+				<CartContext.Provider value={cart}>
 					<Navigation />
 						{/* Routes */}
 						<Route exact path="/">
@@ -39,7 +39,7 @@ function App() {
 						<Route path="/cart">
 							<ShoppingCart />
 						</Route>
-				</CartContext>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
